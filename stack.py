9f5fc1e7ce4cebc64885b551884238e24386dce9
eric9687 <eric9687@naver.com>
2021-11-30 04:24:37 +0900

    11/30

--- stack.py
+++ stack.py
@@ -62,32 +62,104 @@
 
 
 ## 2800
+# import sys
+# from itertools import combinations
+# input = sys.stdin.readline
+
+# S = list(input().rstrip())
+
+# stack = []
+# couple = []
+# answer = set()
+
+# for i in range(len(S)):
+    
+#     if S[i] == '(':
+#         stack.append(i)
+#     elif S[i] == ')':
+#         couple.append((stack.pop(), i))
+
+# for i in range(1,len(couple)+1):
+    
+#     couple_comb = combinations(couple,i)
+#     for j in couple_comb:
+#         tmp = S[:]
+#         for (l,r) in j:
+#             tmp[l] = ''
+#             tmp[r] = ''
+#         answer.add(''.join(tmp))
+# # print(set(answer))
+# for i in sorted(list(answer)):
+#     print(i)
+
+
+## 22866
+
+# import sys
+# input = sys.stdin.readline
+
+# N= int(input())
+# B = [0] + list(map(int,input().split()))
+# stack = [(1,B[1])]
+# Arr = [0]*(N+1)
+# for i in range(2,N+1):
+#     while stack:
+#         if stack[-1][1] < B[i]:
+#             Arr[stack[-1][0]] = i
+#             stack.pop()
+#         else:
+#             break
+#     stack.append((i,B[i])) 
+        
+# print(Arr)
+
+
+
+## 16120
+# import sys
+# input = sys.stdin.readline
+
+# S = input()
+
+# if S == 'P' or S == 'PPAP':
+#     print('PPAP')
+# else: 
+#     stack = []
+#     ppap = ['P','P','A','P']
+#     for ch in S:
+#         stack.append(ch)
+#         if stack[-4:] == ppap:
+#             for _ in range(3):
+#                 stack.pop()
+                
+#     if stack == ['P'] or ppap:
+#         print("PPAP")
+#     else:
+#         print('NP') 
+
+## 6549
 import sys
-from itertools import combinations
 input = sys.stdin.readline
 
-S = list(input().rstrip())
-
-stack = []
-couple = []
-answer = set()
-
-for i in range(len(S)):
+while True:
+    N, *H = list(map(int, input().split()))+[0]
+    if N ==0:
+        break
+    stack = []
+    i = 0
+    ans = 0
+    while i < N+1:
+        if not stack or H[stack[-1]] <= H[i]:
+            stack.append(i)
+            i += 1
+        else:
+            height = H[stack.pop()]
+            if stack:
+                ans = max(ans, height * (i - stack[-1]-1))
+            else:
+                ans = max(ans, height * i)
+    print(ans)
     
-    if S[i] == '(':
-        stack.append(i)
-    elif S[i] == ')':
-        couple.append((stack.pop(), i))
-
-for i in range(1,len(couple)+1):
+        
+            
     
-    couple_comb = combinations(couple,i)
-    for j in couple_comb:
-        tmp = S[:]
-        for (l,r) in j:
-            tmp[l] = ''
-            tmp[r] = ''
-        answer.add(''.join(tmp))
-# print(set(answer))
-for i in sorted(list(answer)):
-    print(i)
