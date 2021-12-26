81e49556c8a63bb59d6c1c48c33505f68b73eb12
eric9687 <eric9687@naver.com>
2021-12-27 00:08:45 +0900

    12/26

--- pr.py
+++
@@ -1,6 +0,0 @@
-def hh(a):
-    if a==1: return True
-
-print(hh(2))
-if hh(2):
-    aprint("*")
--- silgol_rand.py
+++ silgol_rand.py
@@ -37,25 +37,28 @@
 
     
 ## 4307
-import sys
-input = sys.stdin.readline
+# import sys
+# input = sys.stdin.readline
 
-for _ in range(int(input())):
-    stick, n = map(int,input().split())
+# for _ in range(int(input())):
+#     stick, n = map(int,input().split())
     
-    min_ant = []
-    max_ant = []
-    for _ in range(n):
-        ant = int(input())
-        if 2*ant > stick:
-            min_time = stick - ant
-            max_time = ant
-        else:
-            min_time = ant
-            max_time = stick - ant
-        min_ant.append(min_time)
-        max_ant.append(max_time)
-    print(max(min_ant), max(max_ant))
+#     min_ant = []
+#     max_ant = []
+#     for _ in range(n):
+#         ant = int(input())
+#         if 2*ant > stick:
+#             min_time = stick - ant
+#             max_time = ant
+#         else:
+#             min_time = ant
+#             max_time = stick - ant
+#         min_ant.append(min_time)
+#         max_ant.append(max_time)
+#     print(max(min_ant), max(max_ant))
         
         
-        
+print("\\    /\\")
+print(" )  ( \')")
+print("(  /  )")
+print(" \\(__)|")        
